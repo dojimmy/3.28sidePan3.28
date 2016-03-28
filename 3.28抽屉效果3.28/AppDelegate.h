16e4752174edc937d3de175bbe25e2d6f0ed9c42
邓金明 <Jimmy@dengjinmingdemacbook-pro.local>
2016-03-28 19:59:30 +0800

    Initial Commit

---
+++ 3.28抽屉效果3.28/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3.28抽屉效果3.28
+//
+//  Created by 邓金明 on 16/3/28.
+//  Copyright © 2016年 邓金明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3.28抽屉效果3.28/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3.28抽屉效果3.28
+//
+//  Created by 邓金明 on 16/3/28.
+//  Copyright © 2016年 邓金明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
